9969bd5799848fb18e82399a86c265d8d3e76dd2
AshtonPadiachy <102726839+AshtonPadiachy@users.noreply.github.com>
2022-06-02 15:51:33 +0200

    Added class

--- WeatherAppSln/WeatherApp/WeatherApp/MainPage.xaml.cs
+++ WeatherAppSln/WeatherApp/WeatherApp/MainPage.xaml.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
+using static WeatherApp.WeatherInfo;
 
 namespace WeatherApp
 {
@@ -13,6 +17,18 @@ namespace WeatherApp
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private async void GetWeatherInfo()
+        {
+            var location = Geolocation.GetLocationAsync();
+
+            var client = new HttpClient();
+
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+            var response = await client.GetStringAsync("https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6da8fe4c36aa7ecc16ea9a9fd68abc1");
+
+            var weatherInfo = JsonConvert.DeserializeObject<OpenWeatherInfo>(response);
         }
     }
 }
